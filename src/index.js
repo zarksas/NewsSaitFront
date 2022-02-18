b907812e9c34fe1d67f1991e80df3c961517050b
username <nomadicalli@mail.ru>
2022-02-18 10:28:48 +0300

    Footer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-
+// eslint-disable-next-line import/no-unresolved
+import "footer";
+// eslint-disable-next-line import/no-unresolved
+import "style";
+// eslint-disable-next-line import/no-unresolved
+import "index";
